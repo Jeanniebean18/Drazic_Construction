f5dbdefd507374c3d4c6dec2ea857e9fd50ae3ce
Jeannie <jeannie@kickpunchcreative.com>
2015-07-21 22:14:16 -0500

    working more with icons and columns need to add contact form

--- public/global.js
+++ public/global.js
@@ -3,6 +3,8 @@ var header = document.getElementById("header");
 var box = document.getElementById("box");
 var logo = document.getElementById("wrapper__logo");
 var icons = document.getElementById("icons_container");
+var icons2 = document.getElementById("icons_container2");
+var icons3 = document.getElementById("icons_container3");
 
 window.onscroll = shrink_header;
 
@@ -14,8 +16,16 @@ function shrink_header() {
   }
   if (window.pageYOffset>200) {
     show_icons();
-    
   }
+    
+  if (window.pageYOffset>475) {
+    show_icons2();
+  }
+    
+  if (window.pageYOffset>750) {
+    show_icons3();
+  }
+    
   if (window.pageYOffset>700) {
     pop_box();
   }
@@ -32,9 +42,18 @@ function pop_box() {
 function show_icons() {
   icons.style.visibility = "visible"
   icons.setAttribute("class", " animated bounceIn");
-    
-    
 }
+
+function show_icons2() {
+  icons2.style.visibility = "visible"
+  icons2.setAttribute("class", " animated bounceIn");
+}
+
+function show_icons3() {
+  icons3.style.visibility = "visible"
+  icons3.setAttribute("class", " animated bounceIn");
+}
+
 
 
 
