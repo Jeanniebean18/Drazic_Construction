65d35b5883ea02d5fa00419c6da656585ad22b18
Jeannie <jeannie@kickpunchcreative.com>
2015-07-25 06:45:31 -0500

    favicon

--- public/global.js
+++ public/global.js
@@ -11,22 +11,22 @@ window.onscroll = shrink_header;
 
 
 function shrink_header() {
-  if (window.pageYOffset>100) {
-    header.style.visibility = "visible";
-    header.setAttribute("class", " animated slideInDown");
+  if (window.pageYOffset>25) {
+    // header.style.visibility = "visible";
+//     header.setAttribute("class", " animated slideInDown");
   }
-  if (window.pageYOffset>200) {
-    show_icons();
-  }
-    
-  if (window.pageYOffset>475) {
-    show_icons2();
-  }
-    
-  if (window.pageYOffset>750) {
-    show_icons3();
-  }
-    
+  // if (window.pageYOffset>200) {
+  //   show_icons();
+  // }
+  //
+  // if (window.pageYOffset>475) {
+  //   show_icons2();
+  // }
+  //
+  // if (window.pageYOffset>750) {
+  //   show_icons3();
+  // }
+  //
   if (window.pageYOffset>1175) {
     show_form();
     // pop_box();
@@ -34,27 +34,27 @@ function shrink_header() {
 
 }
 
-
-function pop_box() {
- 
-  box.style.display = "block";
-
-}
-
-function show_icons() {
-  icons.style.visibility = "visible"
-  icons.setAttribute("class", " animated bounceIn");
-}
-
-function show_icons2() {
-  icons2.style.visibility = "visible"
-  icons2.setAttribute("class", " animated bounceIn");
-}
-
-function show_icons3() {
-  icons3.style.visibility = "visible"
-  icons3.setAttribute("class", " animated bounceIn");
-}
+//
+// function pop_box() {
+//
+//   box.style.display = "block";
+//
+// }
+//
+// function show_icons() {
+//   icons.style.visibility = "visible"
+//   icons.setAttribute("class", " animated bounceIn");
+// }
+//
+// function show_icons2() {
+//   icons2.style.visibility = "visible"
+//   icons2.setAttribute("class", " animated bounceIn");
+// }
+//
+// function show_icons3() {
+//   icons3.style.visibility = "visible"
+//   icons3.setAttribute("class", " animated bounceIn");
+// }
 
 function show_form() {
   form.style.visibility = "visible"
