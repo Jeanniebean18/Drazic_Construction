4c973c8aec97df52df02bfa9f66696f199db33e4
Jeannie <jeannie@kickpunchcreative.com>
2015-07-25 08:03:16 -0500

    contact form working

--- controllers/contacts.rb
+++ controllers/contacts.rb
@@ -5,3 +5,18 @@ end
 get "/home" do
   erb :"home"
 end
+
+post "/contacts" do
+  @contact = Contact.create({name:params[:name], phone: params[:phone], email:params[:email], message:params[:message], date:params[:date]})
+  if @contact.save
+    redirect "/contacts" #should redirect to login after creating contact.
+  else
+    @error = true
+    redirect "/home"
+  end
+  
+end
+
+get "/contacts" do
+  erb :contacts
+end
